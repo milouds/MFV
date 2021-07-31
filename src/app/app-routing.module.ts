c1f9e491bf2983e14ea5ebf8a5b3c2ca29490b3a
chaieb achraf <chaiebachraf0@gmail.com>
2021-08-01 03:55:53 +1300

    Second commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,9 +8,9 @@ import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   {path:'',component:AccComponent},
-  {path:'rvente',loadChildren:()=>import('./rvente/rvente.module').then(m=>m.RventeModule),
-   //canActivate:[AuthGuard]
-  },
+  {path:'rvente',loadChildren:()=>import('./rvente/rvente.module').then(m=>m.RventeModule)
+
+ },
   {path:'rcomf',loadChildren:()=>import('./rcomf/rcomf.module').then(m=>m.RcomfModule)},
   {path:'Register',component:RegisterComponent},
   {path:'Login',component:LoginComponent},
